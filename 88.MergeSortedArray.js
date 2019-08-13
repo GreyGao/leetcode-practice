b85a508f91577577643b838d4a90826f98ccfcbf
GreyGao <rh87116938@163.com>
2019-08-13 22:18:49 +0800

    88

--- 88.MergeSortedArray.js
+++ 88.MergeSortedArray.js
@@ -21,26 +21,18 @@
  * @param {number} n
  * @return {void} Do not return anything, modify nums1 in-place instead.
  */
-var merge = function (nums1, m, nums2, n) {
+var merge = function ( nums1, m, nums2, n ) {
   nums1.splice(m, nums1.length);
   nums2.splice(n, nums2.length);
 
   let x = 0, y = 0;
-  while (typeof nums1[x] === 'number' || typeof nums2[y] === 'number') {
-    const p = nums1[x];
-    const q = nums2[y];
-
-    console.log(p, q)
-    if (q > p) {
+  while ( y < n ) {
+    if ( nums1[x] >= nums2[y] || nums1[x] !== undefined ) {
+      nums1.splice(x, 0, nums2[y]);
       x++;
-    } else if (q <= p) {
-      nums1.splice(x, 0, q);
       y++;
-    } else if (!p) {
-      nums1.splice(nums1.length, 0, nums2[y]);
-      y++
     } else {
-      x++;
+      x++
     }
   }
 
@@ -48,6 +40,5 @@ var merge = function (nums1, m, nums2, n) {
 };
 
 // console.log(merge([1, 2, 3, 0, 0, 0], 3, [2, 5, 6], 3)); // [1,2,2,3,5,6]
-console.log(merge([-1, -1, 0, 0, 0, 0], 4, [-1, 0], 2)); // [1,2,3,4,5,5,6]
-// console.log(merge([-1, 0, 0, 3, 3, 3, 0, 0, 0], 6, [1, 2, 2], 3)); // [-1,0,0,1,2,2,3,3,3]
-
+// console.log(merge([ -1, -1, 0, 0, 0, 0 ], 4, [ -1, 0 ], 2)); // [-1,-1,-1,0,0,0]
+console.log(merge([ -1, 0, 0, 3, 3, 3, 0, 0, 0 ], 6, [ 1, 2, 2 ], 3)); // [-1,0,0,1,2,2,3,3,3]
