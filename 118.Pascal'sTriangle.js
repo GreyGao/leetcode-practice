0678f4246ad4197531e75ed2539b9e105557bd67
Grey Gao <rh87116938@163.com>
2019-09-17 17:45:54 +0800

    118

---
+++ 118.Pascal'sTriangle.js
@@ -0,0 +1,48 @@
+/**
+ * Given a non-negative integer numRows, generate the first numRows of Pascal's triangle.
+ * In Pascal's triangle, each number is the sum of the two numbers directly above it.
+ Example:
+
+ Input: 5
+ Output:
+ [
+ [1],
+ [1,1],
+ [1,2,1],
+ [1,3,3,1],
+ [1,4,6,4,1]
+ ]
+
+ * */
+
+/**
+ * @param {number} numRows
+ * @return {number[][]}
+ */
+var generate = function (numRows) {
+  if (numRows === 0) return [];
+
+  const result = [[1]];
+  if (numRows === 1) return result;
+
+  let i = 1;
+  let prev = result[0];
+  while (i < numRows) {
+    const nextRow = generateNextRow(prev);
+    result.push(nextRow);
+    prev = nextRow;
+    i++;
+  }
+
+  return result;
+};
+const generateNextRow = (row = []) => {
+  const nextRow = [row[0]];
+  row.forEach((item, i) => {
+    const num = row[i + 1] ? row[i + 1] + item : item;
+    nextRow.push(num);
+  })
+  return nextRow;
+}
+
+console.log(generate(5));
