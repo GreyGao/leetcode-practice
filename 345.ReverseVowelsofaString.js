9ccf8419232cccbe80f0241defd8233760d7c989
Grey Gao <rh87116938@163.com>
2019-09-24 23:16:48 +0800

    345

--- 345.ReverseVowelsofaString.js
+++ 345.ReverseVowelsofaString.js
@@ -17,6 +17,39 @@
  * @param {string} s
  * @return {string}
  */
-var reverseVowels = function(s) {
-
+var reverseVowels = function (s) {
+  /**
+   * 思路：
+   * 前后i、j两个指针
+   * 遇到原音则交换位置
+   * 直到i、j相遇为止
+   * O(n)
+   * Runtime: 80 ms, faster than 59.93% of JavaScript online submissions for Reverse Vowels of a String.
+   * Memory Usage: 38.2 MB, less than 92.86% of JavaScript online submissions for Reverse Vowels of a String.
+   * */
+  const map = {
+    a: true, e: true, i: true, o: true, u: true,
+    A: true, E: true, I: true, O: true, U: true,
+  };
+  const arr = s.split('');
+  let j = arr.length - 1;
+  for (let i = 0; i < arr.length; i++) {
+    if (i === j) break;
+    const char = arr[i];
+    if (map[char]) {
+      while (i < j) {
+        if (map[arr[j]]) {
+          arr[i] = arr[j];
+          arr[j] = char;
+          j--;
+          break;
+        }
+        j--;
+      }
+    }
+  }
+  return arr.join('');
 };
+
+// console.log(reverseVowels('hello')); // holle
+console.log(reverseVowels('leetcode')); // leotcede
