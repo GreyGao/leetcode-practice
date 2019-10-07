6eb0ea2c519f347f553fad5e9ca02f12a19b0539
Grey Gao <rh87116938@163.com>
2019-10-07 22:53:11 +0800

    349

--- 349.IntersectionofTwoArrays.js
+++ 349.IntersectionofTwoArrays.js
@@ -21,5 +21,30 @@
  * @return {number[]}
  */
 var intersection = function(nums1, nums2) {
+  /**
+   * 思路：
+   * 寻找两个数组交叉的部分，之前有误解以为需要连续交叉
+   * 寻找nums1、nums2中更短的数组作为hashMap
+   * 寻找两者交叉重叠的部分
+   * Runtime: 52 ms, faster than 92.15% of JavaScript online submissions for Intersection of Two Arrays.
+   * Memory Usage: 35.6 MB, less than 7.69% of JavaScript online submissions for Intersection of Two Arrays.
+   * */
+  let shorter = nums1, longer = nums2;
+  if(nums1.length > nums2.length) {
+    shorter = nums2;
+    longer = nums1;
+  }
 
+  const result = [];
+  const map = new Set(shorter);
+  for(const j of longer) {
+    if(map.has(j)){
+      result.push(j);
+      map.delete(j);
+    }
+  }
+  return result;
 };
+
+console.log(intersection([1,2,2,1],[2,2])); // [2]
+console.log(intersection([4,9,5],[9,4,9,8,4])); // [9,4]
