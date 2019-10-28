4253badf14d7cc33dbfddbe576b34e6a7380db79
Grey Gao <rh87116938@163.com>
2019-10-28 14:08:27 +0800

    448

--- 448.FindAllNumbersDisappearedinanArray.js
+++ 448.FindAllNumbersDisappearedinanArray.js
@@ -22,12 +22,30 @@ var findDisappearedNumbers = function (nums) {
   /**
    * 思路：
    * 因为需求是不再申请额外的空间，并在O(n)时间内完成;
-   * 将nums进行排序后，进行遍历
-   * 如果当前i!==num, 则将当前i push进入数组尾部
-   * 否则将当前num unshift出去，
-   * 当i=n时，返回数组
+   * 使用index标记的思路
+   * 遍历nums数组，标记当前num如果按升序排列所原本应该处在的位置
+   * 如 -4 -> index = 3
+   * 此时标记nums[3]的num为负数，意为找到了对应下标的原数字
+   * 重新遍历数组，如果当前num>0则意味着数组中不存在当前index所应存在的num
+   * 使用filter过滤函数返回那些缺失的数字
+   * Runtime: 92 ms, faster than 94.90% of JavaScript online submissions for Find All Numbers Disappeared in an Array.
+   * Memory Usage: 43.9 MB, less than 62.50% of JavaScript online submissions for Find All Numbers Disappeared in an Array.
    * */
 
+  // 查找标记已存在的数字
+  nums.forEach((num, i) => {
+    let sortIndex = Math.abs(nums[i]) - 1;
+    nums[sortIndex] = -Math.abs(nums[sortIndex]);
+  });
+  // 替换那些寻找到缺失的数字的原值
+  nums.forEach((num, i) => {
+    if (num > 0) {
+      nums[i] = i + 1;
+    }
+  })
+  // 过滤结果
+  return nums.filter(num => num > 0);
 };
 
 console.log(findDisappearedNumbers([4, 3, 2, 7, 8, 2, 3, 1])); // [5,6]
+console.log(findDisappearedNumbers([4, 3, 4, 5, 1,])); // [2]
